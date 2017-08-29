869ee1ae124dbbec6ed74e1d3ec9be3db0a5c775
Erol Fornoles <erol.fornoles@gmail.com>
2017-08-30 06:08:33 +0800

    Passing the current ranter ID is no longer necessary

--- app/controllers/rants_controller.rb
+++ app/controllers/rants_controller.rb
@@ -1,6 +1,6 @@
 class RantsController < ApplicationController
   def create
-    Rant.create rant_params
+    current_ranter.rants.create rant_params
 
     redirect_to root_path
   end
@@ -8,6 +8,6 @@ class RantsController < ApplicationController
   private
 
   def rant_params
-    params.require(:rant).permit(:ranter_id, :message, :photo)
+    params.require(:rant).permit(:message, :photo)
   end
 end
