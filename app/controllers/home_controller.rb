70cc7070ac81f52a95bd7c0612f8cd5c2907838d
Erol Fornoles <erol.fornoles@gmail.com>
2017-08-30 06:08:10 +0800

    Add logout link

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
+  before_action :ensure_current_user, only: [:index]
+
   def index
     @ranter = current_ranter
+  end
+
+  private
+
+  def ensure_current_user
+    redirect_to new_ranter_session_path unless current_ranter
   end
 end
